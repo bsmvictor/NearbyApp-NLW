723734365a7865295fc8b234e500c6b2e35d6d90
bsmvictor <victor.boaventura@gec.inatel.br>
2024-12-09 23:01:29 -0300

    Criando tela Splash

---
+++ NearbyApp NLW/Features/Coordinator/NearbyFlowController.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+class NearbyFlowController {
+    private var navigationController: UINavigationController?
+    
+    public init() {
+        
+    }
+    
+    func start() -> UINavigationController? {
+        let contentView = SplashView()
+        let startViewController = SplashViewController(contentView: contentView)
+        self.navigationController = UINavigationController(rootViewController: startViewController)
+        
+        return navigationController
+    }
+}
